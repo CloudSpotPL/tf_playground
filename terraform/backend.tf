fb1daff0032bb6a568366d2a93830ff63f5723a2
nietakipawel <43452447+nietakipawel@users.noreply.github.com>
2021-09-30 21:24:26 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,7 +4,6 @@ terraform {
     workspaces {
       name = "playground"
     }
-    token = "uOhcsAaogmXeuw.atlasv1.mp1GFulOTTkrE9Ta89Rlsv7kUKhRe74pXdT51vr5T7SIIgnyBMvq1JsXJoz81epJZQc"
   }
   required_version = ">= 0.13.0"
 }
